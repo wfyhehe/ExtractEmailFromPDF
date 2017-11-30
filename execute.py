3f5973d8e0d41b1e3c2cf3f1c410f0f8439dddc0
wfyhehe <826487285@qq.com>
2017-11-30 21:58:41 +0800

    support docx, but not doc

--- execute.py
+++ execute.py
@@ -1,41 +1,49 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
+import logging
+import os
 
-import os.path
-import csv
 from datetime import datetime
-from utils.from_doc import extract_cellphone_from_doc, extract_email_from_doc
+from utils.from_docx import extract_cellphone_from_docx, extract_email_from_docx
 from utils.from_pdf import extract_cellphone_from_pdf, extract_email_from_pdf
 
-
+now_str = datetime.now().strftime('%Y%m%d%H%M%S')
+OUTPUT_CSV = 'output_csv'
 pdf_list = []
-doc_list = []
+docx_list = []
 emails = []
 phones = []
 
+
 def process_directory(args, dir_name, file_names):
     for file_name in file_names:
-        if file_name.endswith('.pdf'):
+        if file_name.endswith(u'.pdf'):
             pdf_list.append(file_name)
-        elif file_name.endswith('.doc') or file_name.endswith('.docx'):
-            doc_list.append(file_name)
+        elif file_name.endswith(u'.docx'):
+            docx_list.append(file_name)
+        elif file_name.endswith(u'.doc'):
+            logging.error('can\'t process "%s", please convert to .docx or .pdf' % file_name)
 
 
-os.path.walk('.', process_directory, None)
+os.path.walk(u'.', process_directory, None)
 
 for pdf in pdf_list:
     emails.extend(extract_email_from_pdf(pdf))
     phones.extend(extract_cellphone_from_pdf(pdf))
-for doc in doc_list:
-    emails.extend(extract_email_from_doc(doc))
-    phones.extend(extract_cellphone_from_doc(doc))
+for docx in docx_list:
+    emails.extend(extract_email_from_docx(docx))
+    phones.extend(extract_cellphone_from_docx(docx))
 
-email_csv = ', '.join(set(emails))
-phone_csv = ', '.join(set(phones))
-now_str = datetime.now().strftime('%Y%m%d%H%M%S')
+email_csv = ','.join(set(emails))
+phone_csv = ','.join(set(phones))
 
-with open('emails-{datetime}.csv'.format(datetime=now_str), 'w') as f:
+try:
+    os.mkdir(OUTPUT_CSV)
+except:
+    pass
+
+with open('{dir}/emails-{datetime}.csv'.format(dir=OUTPUT_CSV, datetime=now_str), 'w') as f:
     f.write(email_csv)
 
-with open('phones-{datetime}.csv'.format(datetime=now_str), 'w') as f:
+with open('{dir}/phones-{datetime}.csv'.format(dir=OUTPUT_CSV, datetime=now_str), 'w') as f:
     f.write(phone_csv)
--- utils/from_docx.py
+++ utils/from_docx.py
@@ -2,10 +2,9 @@
 # -*- coding: utf-8 -*-
 
 import re
-import docx
+import zipfile
 
-
-def extract_email_from_doc(doc_name):
+def extract_email_from_docx(docx_name):
     """
     doc_name='xxx.doc(x)'
     return a list of emails
@@ -13,17 +12,17 @@ def extract_email_from_doc(doc_name):
     :return list['xxx@xxx.xxx'...]:
     """
     block_text = ''
-    doc = docx.Document(doc_name)
-    paras = doc.paragraphs
-
-    for p in paras:
-        block_text += p.text + '\n'
+    if zipfile.is_zipfile(docx_name):
+        fz = zipfile.ZipFile(docx_name, 'r')
+        for f in fz.namelist():
+            if f.endswith('document.xml'):
+                block_text += fz.read(f)
 
     email_regex = u'([a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+)'
     return re.findall(email_regex, block_text)
 
 
-def extract_cellphone_from_doc(doc_name):
+def extract_cellphone_from_docx(docx_name):
     """
     doc_name='xxx.doc(x)'
     return a list of cellphone numbers
@@ -31,10 +30,11 @@ def extract_cellphone_from_doc(doc_name):
     :return list['xxxxxxxxxx'...]:
     """
     block_text = ''
-    doc = docx.Document(doc_name)
-    paras = doc.paragraphs
-    for p in paras:
-        block_text += p.text
+    if zipfile.is_zipfile(docx_name):
+        fz = zipfile.ZipFile(docx_name, 'r')
+        for f in fz.namelist():
+            if f.endswith('document.xml'):
+                block_text += fz.read(f)
 
     phone_regex = u'\D([1][3,4,5,7,8][0-9]{9})\D'
     return re.findall(phone_regex, block_text)
