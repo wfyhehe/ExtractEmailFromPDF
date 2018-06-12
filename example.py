e3cf99b76f630fb4b56166a1e0847bc25726dfec
wfyhehe <826487285@qq.com>
2018-06-12 15:40:20 +0800

    fix example

--- example.py
+++ example.py
@@ -1,13 +1,13 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-from utils.from_doc import extract_cellphone_from_doc, extract_email_from_doc
+from utils.from_docx import extract_cellphone_from_docx, extract_email_from_docx
 from utils.from_pdf import extract_cellphone_from_pdf, extract_email_from_pdf
 
-emails1 = extract_email_from_doc('email.docx')  # [u'123456@qq.com',...]
+emails1 = extract_email_from_docx('email.docx')  # [u'123456@qq.com',...]
 emails2 = extract_email_from_pdf('email.pdf')  # ['123456@qq.com',...]
 
-phones1 = extract_cellphone_from_doc('phone.docx')  # [u'1333333333',...]
+phones1 = extract_cellphone_from_docx('phone.docx')  # [u'1333333333',...]
 phones2 = extract_cellphone_from_pdf('phone.pdf')  # ['1333333333',...]
 
 print emails1
